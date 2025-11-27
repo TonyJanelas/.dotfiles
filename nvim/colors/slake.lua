18eba8d97e34f7de06386df5632b9f0cc82f552a
Tiago Salema <tiagosalema6@gmail.com>
2025-11-27 23:19:15 +0000

    Adapted the vim config and colour scheme for nvim

--- nvim/colors/slake.lua
+++ nvim/colors/slake.lua
@@ -1,14 +1,11 @@
-
--- Name
+------------------------------------------------------Technical Setup------------------------------------------------------
 vim.g.colors_name = "Slake"
-
--- Clear existing colours
 vim.cmd("highlight clear")
 if vim.fn.exists("syntax_on") then
   vim.cmd("syntax reset")
 end
 
--- Colour Palette
+-------------------------------------------------------Colour Palette------------------------------------------------------
 local colors = {
 	black =      "#121212",
 	darkgrey =   "#212121",
@@ -26,25 +23,82 @@ local colors = {
 	pink =       "#BB3E96",
 }
 
--- UI Colours
-vim.api.nvim_set_hl(0, "LineNr", { fg = colors.grey })
-vim.api.nvim_set_hl(0, "CursorLineNr", { fg = colors.lightgrey })
-vim.api.nvim_set_hl(0, "CursorLine", { bg = colors.darkgrey })
-vim.api.nvim_set_hl(0, "CursorColumn", { bg = colors.darkgrey })
-vim.api.nvim_set_hl(0, "StatusLine", { fg = colors.black, bg = colors.white })
-vim.api.nvim_set_hl(0, "StatusLineNC", { fg = colors.darkgrey, bg = colors.white })
-vim.api.nvim_set_hl(0, "Visual", { bg = colors.grey })
-vim.api.nvim_set_hl(0, "Search", { bg = colors.grey })
+--------------------------------------------------------------UI-----------------------------------------------------------
+vim.api.nvim_set_hl(0, "Cursor", { fg = colors.grey })
+vim.api.nvim_set_hl(0, "CursorIM", { bg=colors.lightgrey })
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg=colors.lightgrey })
+vim.api.nvim_set_hl(0, "CursorLine", { bg=colors.darkgrey })
+vim.api.nvim_set_hl(0, "Directory", { fg=colors.lightblue })
+vim.api.nvim_set_hl(0, "StatusLine", { fg=colors.black, bg=colors.white })
+vim.api.nvim_set_hl(0, "StatusLineNC", { fg=colors.darkgrey, bg=colors.white })
+vim.api.nvim_set_hl(0, "TabLine", { fg=colors.black, bg=colors.darkgrey })
+vim.api.nvim_set_hl(0, "TabLineFill", { bg=colors.black })
+vim.api.nvim_set_hl(0, "TabLineSel", { fg=colors.black, bg=colors.lightgrey })
+vim.api.nvim_set_hl(0, "VertSplit", { fg=colors.grey })
 
--- Highlight Groups
-vim.api.nvim_set_hl(0, "Normal", { fg = colors.light, bg = colors.black })
-vim.api.nvim_set_hl(0, "Comment", { fg = colors.green })
-vim.api.nvim_set_hl(0, "String", { fg = colors.orange })
-vim.api.nvim_set_hl(0, "Number", { fg = colors.red })
-vim.api.nvim_set_hl(0, "Boolean", { fg = colors.blue })
-vim.api.nvim_set_hl(0, "Identifier", { fg = colors.lightblue })
-vim.api.nvim_set_hl(0, "Function", { fg = colors.yellow })
-vim.api.nvim_set_hl(0, "Keyword", { fg = colors.pink })
-vim.api.nvim_set_hl(0, "Type", { fg = colors.lightblue })
-vim.api.nvim_set_hl(0, "Operator", { fg = colors.fg })
-vim.api.nvim_set_hl(0, "Special", { fg = colors.pink })
+---------------------------------------------------------Editor------------------------------------------------------------
+vim.api.nvim_set_hl(0, "Normal", { fg=colors.white, bg=colors.black })
+vim.api.nvim_set_hl(0, "Boolean", { fg=colors.blue })
+vim.api.nvim_set_hl(0, "Comment", { fg=colors.green })
+vim.api.nvim_set_hl(0, "Function", { fg=colors.yellow })
+vim.api.nvim_set_hl(0, "Identifier", { fg=colors.lightblue })
+vim.api.nvim_set_hl(0, "Keyword", { fg=colors.pink })
+vim.api.nvim_set_hl(0, "Number", { fg=colors.red })
+vim.api.nvim_set_hl(0, "String", { fg=colors.orange })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { fg=colors.black })
+vim.api.nvim_set_hl(0, "Folded", { fg=colors.lightgrey })
+vim.api.nvim_set_hl(0, "Ignore", { fg=colors.darkgrey })
+vim.api.nvim_set_hl(0, "IncSearch", { fg=colors.darkgrey, bg=colors.yellow })
+vim.api.nvim_set_hl(0, "MatchParen", { fg=colors.purple })
+vim.api.nvim_set_hl(0, "ModeMsg", {})
+vim.api.nvim_set_hl(0, "Visual", { bg=colors.grey })
+
+--------------------------------------------------------Diff Mode----------------------------------------------------------
+vim.api.nvim_set_hl(0, "DiffAdd", { fg=colors.green })
+vim.api.nvim_set_hl(0, "DiffChange", { fg=colors.orange })
+vim.api.nvim_set_hl(0, "DiffDelete", { fg=colors.red })
+vim.api.nvim_set_hl(0, "DiffText", { bg=colors.grey })
+
+---------------------------------------------------------Alerts------------------------------------------------------------
+vim.api.nvim_set_hl(0, "Error", { fg=colors.white, bg=colors.red })
+vim.api.nvim_set_hl(0, "ErrorMsg", { fg=colors.white, bg=colors.red })
+vim.api.nvim_set_hl(0, "Question", { fg=colors.white })
+vim.api.nvim_set_hl(0, "WarningMsg", { fg=colors.black, bg=colors.yellow })
+
+------------------------------------------------------ Popup Menu----------------------------------------------------------
+vim.api.nvim_set_hl(0, "Pmenu", { fg=colors.white, bg=colors.darkgrey })
+vim.api.nvim_set_hl(0, "PmenuMatch", { fg=colors.white, bg=colors.darkgrey, bold = true })
+vim.api.nvim_set_hl(0, "PmenuMatchSel", { fg=colors.black, bg=colors.grey, bold = true })
+vim.api.nvim_set_hl(0, "PmenuSbar", { bg=colors.black })
+vim.api.nvim_set_hl(0, "PmenuSel", { fg=colors.black, bg=colors.grey })
+vim.api.nvim_set_hl(0, "PmenuThumb", { bg=colors.grey })
+
+--------------------------------------------------------Spelling-----------------------------------------------------------
+vim.api.nvim_set_hl(0, "SpellBad", { fg=colors.red, underline = true })
+vim.api.nvim_set_hl(0, "SpellCap", { fg=colors.white, underline = true })
+vim.api.nvim_set_hl(0, "SpellLocal", { fg=colors.yellow, underline = true })
+vim.api.nvim_set_hl(0, "SpellRare", { fg=colors.pink, underline = true })
+
+-------------------------------------------------------Group Links---------------------------------------------------------
+vim.api.nvim_set_hl(0, "LineNr", { link = "Cursor" })
+vim.api.nvim_set_hl(0, "MoreMsg", { link = "Cursor" })
+vim.api.nvim_set_hl(0, "SpecialKey", { link = "Cursor" })
+vim.api.nvim_set_hl(0, "ColorColumn", { link = "CursorLine" })
+vim.api.nvim_set_hl(0, "CursorColumn", { link = "CursorLine" })
+vim.api.nvim_set_hl(0, "FoldColumn", { link = "Folded" })
+vim.api.nvim_set_hl(0, "NonText", { link = "Folded" })
+vim.api.nvim_set_hl(0, "Operator", { link = "Function" })
+vim.api.nvim_set_hl(0, "QuickFixLine", { link = "Function" })
+vim.api.nvim_set_hl(0, "SignColumn", { link = "Identifier" })
+vim.api.nvim_set_hl(0, "Type", { link = "Identifier" })
+vim.api.nvim_set_hl(0, "Special", { link = "Keyword" })
+vim.api.nvim_set_hl(0, "Statement", { link = "Keyword" })
+vim.api.nvim_set_hl(0, "PreProc", { link = "MatchParen" })
+vim.api.nvim_set_hl(0, "Terminal", { link = "Normal" })
+vim.api.nvim_set_hl(0, "Conceal", { link = "Number" })
+vim.api.nvim_set_hl(0, "Label", { link = "Number" })
+vim.api.nvim_set_hl(0, "Todo", { link = "Number" })
+vim.api.nvim_set_hl(0, "Underlined", { link = "SpellCap" })
+vim.api.nvim_set_hl(0, "Search", { link = "Visual" })
+vim.api.nvim_set_hl(0, "VisualNOS", { link = "Visual" })
+vim.api.nvim_set_hl(0, "WildMenu", { link = "WarningMsg" })
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,80 +1,85 @@
--- -----------------------------------------------------------------------------------------------
--- General configuration
--- -----------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------
+--														TECHNICAL
+---------------------------------------------------------------------------------------------------------------------------
 
--- Basic settings
-vim.opt.hlsearch = true
-vim.opt.number = true
-vim.opt.mouse = ""
-vim.opt.showmode = false
+--								------------------------General--------------------------
 vim.opt.spelllang = "en_gb"
-vim.opt.title = true
-vim.opt.titlestring = "nvim"
+vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.expand("~/.vim/undo")
 
--- Leader (this is here so plugins etc pick it up)
+--								-------------------------Input---------------------------
+vim.opt.mouse = ""
 vim.g.mapleader = ","
 
--- Disable native file browser (netrw)
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- Use system clipboard
-vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
-
--- Display settings
-vim.opt.termguicolors = true --Enable true colour
-vim.o.background = "dark"
-
--- Scrolling and UI settings
-vim.opt.cursorline = true
-vim.opt.cursorcolumn = true
-vim.opt.signcolumn = 'yes'
-vim.opt.wrap = false
-vim.opt.sidescrolloff = 8
-vim.opt.scrolloff = 8
-
--- Persist undo (persists your undo history between sessions)
-vim.opt.undodir = vim.fn.stdpath("cache") .. "/undo"
-vim.opt.undofile = true
-
--- Tab stuff
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+--								-------------------------Editor--------------------------
+vim.opt.smartindent = true
 vim.opt.expandtab = false
-vim.opt.autoindent = true
-
--- Search configuration
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.gdefault = true
-
--- open new split panes to right and below (as you probably expect)
-vim.opt.splitright = true
+vim.opt.wrap = false
+vim.opt.shiftwidth = 2
 vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.tabstop = 2
 
--- LSP
-vim.lsp.inlay_hint.enable(true)
+--								-------------------------Search--------------------------
+vim.opt.gdefault = true
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.incsearch = true
+vim.opt.smartcase = true
 
--- Colourscheme
+--								------------------------Folding---------------------------
+vim.opt.foldenable = true
+vim.opt.foldmethod = "indent"
+vim.opt.foldlevel = 99
+vim.opt.foldcolumn = "1"
+
+---------------------------------------------------------------------------------------------------------------------------
+--														DISPLAY
+---------------------------------------------------------------------------------------------------------------------------
+
+--								--------------------------UI-----------------------------
+vim.opt.background = "dark"
+vim.opt.cmdheight = 1
+vim.opt.laststatus = 2
+vim.opt.relativenumber = true
+vim.opt.ruler = true
+vim.opt.showmode = false
+vim.opt.number = true
+vim.opt.showcmd = true
+vim.opt.termguicolors = true
+vim.opt.title = true
+vim.opt.titlestring = "Vim: %f"
+
+--								-------------------------Editor--------------------------
+vim.opt.cursorcolumn = true
+vim.opt.cursorline = true
+vim.opt.listchars = "tab:→→,space:·,eol:¶,trail:~"
+vim.opt.scrolloff = 12
+vim.opt.sidescrolloff = 16
+vim.opt.signcolumn = "yes"
+
+--								---------------------Colour Scheme-----------------------
 vim.cmd("colorscheme slake")
 
--- -----------------------------------------------------------------------------------------------
--- Keymap settings
--- -----------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------
+--														KEYBINDS
+---------------------------------------------------------------------------------------------------------------------------
 
--- Basic keys
-vim.keymap.set("n", "<space>", ":")  -- hit <space> to start a command, quicker than :
-vim.keymap.set("n", "q", "<C-r>")    -- "u" is undo, I map "q" to redo
+--								------------------------General---------------------------
+vim.keymap.se("n", "<space>", ":", { noremap = true, silent = true })
+vim.keymap.se("n", "q", "<C-r>", { noremap = true, silent = true })
 
--- Search navigation
-vim.keymap.set("n", "n", "v:searchforward ? 'n' : 'N'", { expr = true }) -- Forwards
-vim.keymap.set("n", "N", "v:searchforward ? 'N' : 'n'", { expr = true }) -- Backwards
+--								------------------------Search----------------------------
+vim.keymap.se("n", "n", "v:searchforward ? 'n' : 'N'", { expr = true, noremap = true })
+vim.keymap.se("n", "N", "v:searchforward ? 'N' : 'n'", { expr = true, noremap = true })
 
--- toggle line numbers and wrap
-vim.keymap.set("n", "<leader>n", ":set nonumber! relativenumber!<CR>")
+--								------------------------Leader----------------------------
+vim.keymap.se("n", "<leader>n", ":set nonumber! relativenumber!<CR>", { noremap = true, silent = true })
+vim.keymap.se("n", "<leader>e", ":Explore<CR>", { noremap = true, silent = true })
 
--- Moving between splits and resizing
-vim.keymap.set("n", "<C-j>", "<C-W><C-J>")  -- use Ctrl-j (and so on) to move between splits
-vim.keymap.set("n", "<C-k>", "<C-W><C-K>")
-vim.keymap.set("n", "<C-l>", "<C-W><C-L>")
-vim.keymap.set("n", "<C-H>", "<C-W><C-H>")
+--								------------------------Windows---------------------------
+vim.keymap.se("n", "<C-j>", "<C-W><C-J>", { noremap = true, silent = true })
+vim.keymap.se("n", "<C-k>", "<C-W><C-K>", { noremap = true, silent = true })
+vim.keymap.se("n", "<C-l>", "<C-W><C-L>", { noremap = true, silent = true })
+vim.keymap.se("n", "<C-h>", "<C-W><C-H>", { noremap = true, silent = true })
